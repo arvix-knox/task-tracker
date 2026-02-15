7960c67575f2246cd96c7b8483eba3c018c7e4e9
Dastan <torqcd@gmail.com>
2026-02-15 12:15:50 +0300

    add config.py file

---
+++ app/config.py
@@ -0,0 +1,15 @@
+from pydantic import BaseSettings, SettingsConfigDict
+
+class Settings(BaseSettings):
+    DATABASE_URL: str
+    SECRET_KEY: str
+    ALGORITHM: str
+    ACCESS_TOKEN_EXPIRE_MINUTES: int
+    REFRESH_TOKEN_EXPIRE_DAYS: int
+    APP_NAME: str
+    DEBUG: bool
+    model_config = SettingsConfigDict(
+        env_file=".env"
+        env_file_encoding="utf-8"
+        case_sensitive=True    
+    )
