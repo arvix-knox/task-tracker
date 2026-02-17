12f72b7db365eb010407c8a039036b43f1666c43
Dastan <torqcd@gmail.com>
2026-02-17 16:05:04 +0300

    refactoring __init__.py

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -1,11 +1,7 @@
 from app.models.user import User
 from app.models.task import Task
-from app.models.habit import Habit, HabitCompletion
+
 __all__ = [
     "User",
     "Task",
-    "Habit",
-    "HabitCompletion",
-    
-    
     ]
