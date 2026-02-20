d64aba717a9e472e046bd335a51e651589006d9f
JolyRed <jolywolf.22341@mail.ru>
2026-02-20 18:47:17 +0100

    Add migrations

--- alembic/env.py
+++ alembic/env.py
@@ -9,6 +9,8 @@ from sqlalchemy.ext.asyncio import async_engine_from_config
 from app.config import settings
 from app.db.base import Base
 from app.models import User, Task  # noqa: F401
+from app.models.workspace import Workspace
+from app.models.workspacemember import WorkspaceMember
 
 config = context.config
 config.set_main_option("sqlalchemy.url", settings.DATABASE_URL)
